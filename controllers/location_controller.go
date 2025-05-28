8aea568b5549131f3804902376fa22e8ecf64523
Nikhil143731 <93185721+Nikhil143731@users.noreply.github.com>
2025-05-28 12:09:36 +0530

    location modification

--- controllers/location_controller.go
+++ controllers/location_controller.go
@@ -700,7 +700,7 @@ func GetAllLocations(c *gin.Context) {
 	}
 
 	var locations []database.Location
-	if err := database.DB.Preload("ZipCodes").Find(&locations).Error; err != nil {
+	if err := database.DB.Find(&locations).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch locations"})
 		return
 	}
